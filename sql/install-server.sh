38e16816803e17e202debca4064eaf5bd2ced301
Alex Dorogensky <alex@exebar.com>
2023-08-19 04:27:31 -0700

    sql/set empty password for mysql server

--- sql/install-server.sh
+++ sql/install-server.sh
@@ -1,1 +1,1 @@
-podman run --name mysql-box -e MYSQL_ROOT_PASSWORD=test -d --network podman1 docker.io/mysql:latest
+podman run --name mysql-box -e MYSQL_ALLOW_EMPTY_PASSWORD=true -d --network podman1 docker.io/mysql:latest
