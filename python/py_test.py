8f26f3bb4968f48782842eaae0bd083bf7aca11b
Alex Dorogensky <alex@exebar.com>
2023-09-02 19:26:19 -0700

    py_test.py -- add test_str_lower()

--- python/py_test.py
+++ python/py_test.py
@@ -7,6 +7,8 @@ class PyTests(unittest.TestCase):
         self.assertEqual(1, 1)
     def test_str_upper(self):
         self.assertEqual("HELLO", "hello".upper())
+    def test_str_lower(self):
+        self.assertEqual("hello", "HEllo".lower())
 
 if __name__ == '__main__':
     unittest.main()
