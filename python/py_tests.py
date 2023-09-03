81f52ee2a94b368eeabec99f1710c3c55c800219
Alex Dorogensky <alex@exebar.com>
2023-09-02 20:07:52 -0700

    py_tests.py -- add test_str_del()

--- python/py_tests.py
+++ python/py_tests.py
@@ -21,5 +21,10 @@ class PyTests(unittest.TestCase):
         append_return = names.append("James")
         self.assertEqual(["John", "Mike", "James"], names)
         self.assertEqual(None, append_return)
+    def test_list_del(self):
+        names = ["John", "Mike"]
+        del names[0] # wont return None
+        self.assertEqual(["Mike"], names)
+
 if __name__ == '__main__':
     unittest.main()
