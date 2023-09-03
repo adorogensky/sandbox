def49dadd9729298f279a3f53f722953f6922a9d
Alex Dorogensky <alex@exebar.com>
2023-09-02 20:31:00 -0700

    hello_world.py -- just leave print statement

--- python/hello_world.py
+++ python/hello_world.py
@@ -1,20 +1,2 @@
 #!/usr/bin/python3
-msg = "hello, world!"
-print(msg)
-print(msg.title())
-print(msg.upper())
-print(msg.lower())
-print("Hello" + ", World!") #string concatenation
-name = "Alex"
-print(f"Hello {name}")
-names = ['John', 'Mike']
-print(names)
-names.append('James')
-print(names)
-names.insert(0, 'Daniel')
-print(names)
-del names[0]
-print(f"names = {names}")
-names.remove('John')
-print(f"names = {names}")
-
+print("Hello, World!")
