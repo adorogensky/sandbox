aafbaea9ec4d96bbb1cba62d2685ca3551d739a4
Alex Dorogensky <alex@exebar.com>
2023-09-02 19:23:36 -0700

    py_tests.py -- add test_str_upper()

--- python/py_test.py
+++ python/py_test.py
@@ -2,9 +2,11 @@
 import unittest
 
 class PyTests(unittest.TestCase):
-    def test2(self):
-        print("Hello World!\n")
+    def test_print(self):
+        print("Hello World!")
         self.assertEqual(1, 1)
+    def test_str_upper(self):
+        self.assertEqual("HELLO", "hello".upper())
 
 if __name__ == '__main__':
     unittest.main()
