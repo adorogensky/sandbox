ff8100d1fe480a628be3583b8200162abd9a936b
Alex Dorogensky <alex@exebar.com>
2023-10-24 13:42:48 -0700

    py_str_tests.py -- break down tests

--- python/py_str_tests.py
+++ python/py_str_tests.py
@@ -11,27 +11,36 @@ import unittest
 class PyStrTests(unittest.TestCase):
     def test_str_len(self):
         self.assertEqual(5, len("hello"))    
-    def test_str_char_at(self):
+    def test_str_char_at_positive_idx(self):
         self.assertEqual('h', "hello"[0])
+    def test_str_char_at_negative_idx(self):
         self.assertEqual('o', "hello"[-1]) 
-    def test_str_sub_str(self):
+    def test_str_slice_positive_start_idx(self):
         self.assertEqual("hello", "hello"[0:])
+    def test_str_slice_positive_end_idx(self):
         self.assertEqual("", "hello"[:0])
+    def test_str_slice_positive_start_and_end_idx(self):
         self.assertEqual("el", "hello"[1:3])
+    def test_str_slice_negative_start_and_end_idx(self):
         self.assertEqual("ll", "hello"[-3:-1])
+    def test_str_slice_negative_end_idx(self):
         self.assertEqual("lo", "hello"[-2:])
     def test_str_concat(self):
         self.assertEqual("Hello" + " World!", "Hello World!")
-    def test_str_f_str(self):
+    def test_f_str(self):
         name = "Alex"
         self.assertEqual(f"Hello {name}", "Hello Alex")
-    def test_str_find(self):
+    def test_str_find_when_findable(self):
         self.assertEqual(2, 'hello'.find('l'))
+    def test_str_find_when_findable_starting_idx(self):
         self.assertEqual(3, 'hello'.find('l', 3))
+    def test_str_find_when_not_findable(self):
         self.assertEqual(-1, 'hello'.find('H'))
-    def test_str_index(self):
+    def test_str_index_when_findable(self):
         self.assertEqual(2, 'hello'.index('l'))
+    def test_str_index_when_findable_starting_idx(self):
         self.assertEqual(3, 'hello'.index('l', 3))
+    def test_str_index_when_not_findable(self):
         with self.assertRaises(ValueError):
             'hello'.index('H')
     def test_str_title(self):
