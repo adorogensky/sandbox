43a01db4401a6ba2cba70915af762fbc980cd110
Alex Dorogensky <alex@exebar.com>
2023-11-06 19:09:19 -0800

    003_str_palindrome.py -- add description

--- python/problems/003_str_palindrome.py
+++ python/problems/003_str_palindrome.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 import unittest
 
+'''
+Given the string, check if it is a palindrome, i.e.
+a string that doesn't change when reversed, i.e.
+it reads the same backward and forward
+
+Examples:
+"eye" is a palindrome
+"noon" is a palindrome
+"decaf faced" is a palindrome
+"taco cat" is not a palindrome (backwards it spells "tac ocat")
+"racecars" is not a palindrome (backwards it spells "sracecar")
+'''
 def solution(_str):
     str_len = len(_str)
     for i in range(0, str_len // 2):
