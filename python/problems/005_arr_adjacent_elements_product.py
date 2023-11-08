fa7a9169dc0e3880496873b390cc64824e463339
Alex Dorogensky <alex@exebar.com>
2023-11-07 18:15:03 -0800

    005_arr_adjacent_elements_product.py -- update desc

--- python/problems/005_arr_adjacent_elements_product.py
+++ python/problems/005_arr_adjacent_elements_product.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 '''
-Given an array of integers, find the pair of adjacent elements that 
-has the largest product and return that product.
+Given an array of integers, find the pair of adjacent elements
+that has the largest product and return that product
 
 Example:
 For inputArray = [3, 6, -2, -5, 7, 3], the output should be
-solution(inputArray) = 21.
+solution(inputArray) = 21
 '''
 import unittest
 
