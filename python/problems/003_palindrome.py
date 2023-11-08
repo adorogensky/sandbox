fd710b59f1ce0e308adfa03ac7effbbc71d26708
Alex Dorogensky <alex@exebar.com>
2023-11-07 17:45:21 -0800

    003_palindrome.py -- add new tests, correct existing impl, add new impl

--- python/problems/003_palindrome.py
+++ python/problems/003_palindrome.py
@@ -2,35 +2,69 @@
 import unittest
 
 '''
-Given the string, check if it is a palindrome, i.e.
-a string that doesn't change when reversed, i.e.
-it reads the same backward and forward
+Check if a string is a palindrome, i.e.
+when it reads the same backward and forward (excluding spaces)
 
 Examples:
 "eye" is a palindrome
 "noon" is a palindrome
 "decaf faced" is a palindrome
-"taco cat" is not a palindrome (backwards it spells "tac ocat")
-"racecars" is not a palindrome (backwards it spells "sracecar")
+"taco cat" is a palindrome (it spells "tacocat" backwards excluding spaces)
+"racecars" is not a palindrome ("racecars" != "sracecar")
+"borrow or rob" is a palindrome ("borrow or rob" == "borroworrob")
 '''
 def solution(_str):
-    str_len = len(_str)
+    _str_no_spaces = _str.replace(" ", "")
+    str_len = len(_str_no_spaces)
     for i in range(0, str_len // 2):
-        if _str[i] != _str[str_len - 1 - i]:
+        if _str_no_spaces[i] != _str_no_spaces[str_len - 1 - i]:
             return False
     return True
 
+def solution2(_str):
+    str_len = len(_str)
+    leftIdx = 0
+    rightIdx = str_len - 1
+
+    while leftIdx <= rightIdx:
+        if _str[leftIdx] != ' ' and _str[rightIdx] != ' ':
+            if _str[leftIdx] != _str[rightIdx]:
+                return False
+            else:
+                leftIdx = leftIdx + 1
+                rightIdx = rightIdx - 1
+        elif _str[leftIdx] == ' ' and _str[rightIdx] == ' ':
+            leftIdx = leftIdx - 1
+            rightIdx = rightIdx - 1
+        elif _str[leftIdx] == ' ':
+            leftIdx = leftIdx + 1
+        elif _str[rightIdx] == ' ':
+            rightIdx = rightIdx - 1
+
+    return True
+    
 class SolutionTests(unittest.TestCase):
     def test_1(self):
         self.assertTrue(solution('eye'))
+        self.assertTrue(solution2('eye'))
     def test_2(self):
         self.assertTrue(solution('noon'))
+        self.assertTrue(solution2('noon'))
     def test_3(self):
         self.assertTrue(solution('decaf faced'))
+        self.assertTrue(solution2('decaf faced'))
     def test_4(self):
-        self.assertFalse(solution('taco cat'))
+        self.assertTrue(solution('taco cat'))
+        self.assertTrue(solution2('taco cat'))
     def test_5(self):
         self.assertFalse(solution('racecars'))
+        self.assertFalse(solution2('racecars'))
+    def test_6(self):
+        self.assertTrue(solution('borrow or rob'))
+        self.assertTrue(solution2('borrow or rob'))
+    def test_7(self):
+        self.assertTrue(solution('  xx'))
+        self.assertTrue(solution2('  xx'))
 
 if __name__ == '__main__':
     unittest.main()
