54869f61b09992e811182b555f3c72cedea7ced7
Alex Dorogensky <alex@exebar.com>
2023-11-27 17:28:07 -0800

    008_arr_max_seq_1s.py -- add problem description and update var name

--- python/problems/008_arr_max_seq_1s.py
+++ python/problems/008_arr_max_seq_1s.py
@@ -1,28 +1,31 @@
-def find_max_ones_seq(nums):
-    g_ans = 0
+"""
+In an array of numbers find max sequence of 1s
+"""
+def max_seq_1s(nums):
+    ans = 0
     for i in range(len(nums)):
         if nums[i] != 1: continue
-        l_ans = 0
+        c_ans = 0
         while i < len(nums) and nums[i] == 1:
-            l_ans += 1
+            c_ans += 1
             i += 1
-        g_ans = max(g_ans, l_ans)
-    return g_ans
+        ans = max(ans, c_ans)
+    return ans
 
 def test_01():
-    assert find_max_ones_seq([]) == 0
+    assert max_seq_1s([]) == 0
 
 def test_02():
-    assert find_max_ones_seq([1]) == 1
+    assert max_seq_1s([1]) == 1
 
 def test_03():
-    assert find_max_ones_seq([1, 1]) == 2
+    assert max_seq_1s([1, 1]) == 2
 
 def test_04():
-    assert find_max_ones_seq([0, 0]) == 0
+    assert max_seq_1s([0, 0]) == 0
 
 def test_05():
-    assert find_max_ones_seq([0, 0, 1]) == 1
+    assert max_seq_1s([0, 0, 1]) == 1
 
 def test_06():
-    assert find_max_ones_seq([0, 0, 1, 2, 2, 1, 1, 1]) == 3
+    assert max_seq_1s([0, 0, 1, 2, 2, 1, 1, 1]) == 3
