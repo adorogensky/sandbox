e782bb1a16691dd3ffa17cd6742ab53401175963
Alex Dorogensky <alex@exebar.com>
2023-12-05 21:05:38 -0800

    001a_sum.py -- pytest

--- python/problems/001a_sum.py
+++ python/problems/001a_sum.py
@@ -1,19 +1,11 @@
-#!/usr/bin/python3
-import unittest
 '''
-Write a function that returns the sum of two numbers
-Example:
-    For param1 = 1 and param2 = 2, the output should be
-    solution(param1, param2) = 3
+Calculate the sum of two numbers
 '''
-
-def solution(param1, param2):
+def sum(param1, param2):
     return param1 + param2
 
-class SolutionTests(unittest.TestCase):
-    def test_1(self):
-        self.assertEqual(solution(0, 0), 0)
-        
-if __name__ == '__main__':
-    unittest.main()
+def test_0_0():
+    assert sum(0, 0) == 0
 
+def test_1_n1():
+    assert sum(1, -1) == 0
