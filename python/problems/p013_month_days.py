711692926135dd0cc7a64163a8db3cbba2513bb5
Alex Dorogensky <alex@exebar.com>
2023-12-11 15:19:12 -0800

    p013_month_days.py -- refactor simply #2

--- python/problems/p013_month_days.py
+++ python/problems/p013_month_days.py
@@ -3,12 +3,9 @@ def _is_leap_year(year: int) -> int:
     if year % 100 != 0: return True
     return True if year % 400 == 0 else False
 
-def _month_days(year: int, month: int, is_jan_jul: bool) -> int:
-    if month > 7:
-        return _month_days(year, month - 7, is_jan_jul)
-    if month % 2 == 1: return 31
-    if month == 2 and is_jan_jul: return 29 if _is_leap_year(year) else 28
-    return 30
-
 def month_days(year: int, month: int) -> int:
-    return _month_days(year, month, month < 8) if month > 0 and month < 13 else None
+    if month < 1 or month > 12 or year < 1: return None
+    seq = month - 7 if month > 7 else month
+    if seq % 2 == 1: return 31
+    if month == 2: return 29 if _is_leap_year(year) else 28
+    return 30
